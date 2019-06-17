ea010ec1a31a6f0ebf3d477ad683bba92bc2e010
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-17 11:57:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
     @word = word
   end
 
-  def match(word_array)
-    word_array.select do |word|
+  def match(array)
+    array.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
