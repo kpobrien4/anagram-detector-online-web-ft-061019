0043bac63c4511276034a984c42aa2a6784af8a0
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-17 11:57:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 class Anagram
+
   attr_accessor :word
 
   def initialize(word)
-      @word = word
-    end
+    @word = word
+  end
 
-  def self.match(array)
-    array.select do |word|
+  def match(word_array)
+    word_array.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
-
 end
